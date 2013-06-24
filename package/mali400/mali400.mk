85fa776d7a4ac3e7e2d006cd28386157151dfcf2
Miroslav Bendík <miroslav.bendik@gmail.com>
2013-06-24 22:08:26 +0200

    Added mali400 source.

---
+++ package/mali400/mali400.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# mali400
+#
+################################################################################
+
+MALI400_VERSION = c2491fe952354ba44538064e534ed7c731cedb1e
+MALI400_SOURCE = mali_$(MALI400_VERSION).tar.gz
+MALI400_SITE = http://github.com/linux-sunxi/sunxi-mali/tarball/$(MALI400_VERSION)
+MALI400_PROPRIETARY_VERSION = e4ced471d576708ac9aa093e41a0f91cf429862b
+MALI400_PROPRIETARY_SOURCE = mali_$(MALI400_VERSION).tar.gz
+
+MALI400_INSTALL_STAGING = YES
+
+define MALI400_BUILD_CMDS
+	wget http://github.com/linux-sunxi/sunxi-mali-proprietary/tarball/$(MALI400_PROPRIETARY_VERSION)/mali.tar.gz -O $(@D)/mali.tar.gz
+	tar -xvzf $(@D)/mali.tar.gz -C $(@D)
+	rm -rf $(@D)/lib/mali
+	mv $(@D)/linux-sunxi-sunxi-mali-proprietary-e4ced47 $(@D)/lib/mali
+	$(TARGET_MAKE_ENV) $(MAKE1) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) config VERSION=r3p1 ABI=armhf EGL_TYPE=framebuffer
+	$(TARGET_MAKE_ENV) $(MAKE1) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+endef
+
+define MALI400_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE1) DESTDIR=$(STAGING_DIR) -C $(@D) install
+endef
+
+$(eval $(generic-package))
