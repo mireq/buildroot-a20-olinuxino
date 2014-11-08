6b83fb6caac133b6e03d5923e8f746dd886a2954
Miroslav Bendík <miroslav.bendik@gmail.com>
2014-11-08 14:15:08 +0100

    Updated pillow.

--- package/python-pillow/python-pillow.mk
+++ package/python-pillow/python-pillow.mk
@@ -1,31 +1,40 @@
 ################################################################################
 #
-# python-bottle
+# python-pillow
 #
 ################################################################################
 
-PYTHON_PILLOW_VERSION = 2.1.0
+PYTHON_PILLOW_VERSION = 2.4.0
 PYTHON_PILLOW_SOURCE = Pillow-$(PYTHON_PILLOW_VERSION).zip
 PYTHON_PILLOW_SITE = https://pypi.python.org/packages/source/P/Pillow/
-PYTHON_PILLOW_DEPENDENCIES = python zlib freetype jpeg tiff host-python-setuptools host-python-distutilscross
+PYTHON_PILLOW_DEPENDENCIES = python zlib freetype jpeg host-python-setuptools
+PYTHON_PILLOW_SETUP_TYPE = setuptools
 
 define PYTHON_PILLOW_EXTRACT_CMDS
-	(unzip $(DL_DIR)/$(PYTHON_PILLOW_SOURCE) -d $(BUILD_DIR); \
+	(unzip -o $(DL_DIR)/$(PYTHON_PILLOW_SOURCE) -d $(BUILD_DIR); \
 	mv $(BUILD_DIR)/Pillow-$(PYTHON_PILLOW_VERSION)/* $(@D))
+#	echo "[build_ext]" >> $(@D)/setup.cfg
+#	echo "disable-webp = " >> $(@D)/setup.cfg
+#	echo "disable-lcms = " >> $(@D)/setup.cfg
+#	echo "disable-freetype = " >> $(@D)/setup.cfg
+#	echo "disable-jpeg2000 = " >> $(@D)/setup.cfg
+#	echo "disable-tcl = " >> $(@D)/setup.cfg
+#	echo "disable-webpmux = " >> $(@D)/setup.cfg
+#	echo "disable-tiff = " >> $(@D)/setup.cfg
 endef
 
-define PYTHON_PILLOW_BUILD_CMDS
+define PYTHON_PILLOW_BUILD_CMDS__
 	(cd $(@D); \
 		PYTHONXCPREFIX="$(STAGING_DIR)/usr/" \
 		LDFLAGS="-L$(STAGING_DIR)/lib -L$(STAGING_DIR)/usr/lib" \
-	$(HOST_DIR)/usr/bin/python setup.py build -x build_ext --disable-lcms --disable-webp)
+	$(HOST_DIR)/usr/bin/python setup.py build build_ext --disable-lcms --disable-webp)
 endef
 
-define PYTHON_PILLOW_INSTALL_TARGET_CMDS
+define PYTHON_PILLOW_INSTALL_TARGET_CMDS____
 	(cd $(@D); \
 	PYTHONPATH=$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
 	$(HOST_DIR)/usr/bin/python setup.py install \
 	--single-version-externally-managed --root=/ --prefix=$(TARGET_DIR)/usr)
 endef
 
-$(eval $(generic-package))
+$(eval $(python-package))
